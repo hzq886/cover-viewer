c461733094320e57798aa220467c70550d1d3f00
hzq886 <hzq886@gmail.com>
2025-10-05 21:14:48 +0900

    项目追加analytics支持

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/next';
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
@@ -44,6 +45,7 @@ export default async function RootLayout({
       >
         <Providers initialLanguage={initialLanguage}>{children}</Providers>
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
